f02396592f6f67c14bf6aa082a8e0e438f1bd68b
Abbbyccc <abbychien@hotmail.com>
2022-11-14 22:51:43 +0000

    add param to shown on nutrition page

--- pages/[id].js
+++ pages/[id].js
@@ -1,14 +1,19 @@
 // https://api.edamam.com/api/nutrition-data?app_id=cd596177&app_key=%20d3aae1ee2f6f3ae55aebc8f0d4c2662c&nutrition-type=logging&ingr=beetroot
 import { loadVeg } from '../lib/loadVeg.js'
+import Nutrition from '../components/Nutrition.js'
+import { loadNutritionFacts } from '../lib/loadNutritionFacts.js'
 
-const currentMonth = new Date().getMonth()
 export async function getStaticPaths() {
     const allveg = await loadVeg()
-    const currentMonthData = allveg[currentMonth]
-    const allVegArray = currentMonthData.food.map((veg) => veg.name)
-
-    const path = allVegArray.map((veg) => ({
-        params: { id: String(veg).toLocaleLowerCase() },
+    const allYearArray = []
+    for (const month of allveg) {
+        allYearArray.push(month.food.map((veg) => veg.name))
+    }
+    const flatPath = allYearArray.flat()
+    const path = flatPath.map((veg) => ({
+        params: {
+            id: String(veg).toLowerCase().replace(' ', '-'),
+        },
     }))
 
     return {
@@ -18,23 +23,68 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    console.log(params.id)
-    const res = await fetch(
-        `https://api.edamam.com/api/nutrition-data?app_id=cd596177&app_key=%20d3aae1ee2f6f3ae55aebc8f0d4c2662c&nutrition-type=logging&ingr=${params.id}`
-    )
+    console.log(params)
+    const vegetables = await loadNutritionFacts({ params })
 
-    const vegetables = await res.json()
+    // const res = await fetch(
+    //     `https://api.edamam.com/api/nutrition-data?app_id=cd596177&app_key=%20d3aae1ee2f6f3ae55aebc8f0d4c2662c&nutrition-type=logging&ingr=${params.id}`
+    // )
+
+    // if (!res.ok) {
+    //     const message = `An error has occured: ${res.status}`
+    //     throw new Error(message)
+    // }
+    // const vegetables = await res.json()
 
     return {
-        props: vegetables,
-        revalidate: 10,
+        props: {
+            vegetables: vegetables['ingredients'][0]['parsed'][0]['nutrients'],
+            params: params.id,
+        },
+        revalidate: 6000,
     }
 }
 
-export default function SingleVeg() {
+// loadRecipes().catch((error) => {
+//     error.message // 'An error has occurred: 404'
+// })
+
+export default function SingleVeg({ vegetables, params }) {
+    const arr = [
+        vegetables.VITD,
+        vegetables.ENERC_KCAL,
+        vegetables.VITA_RAE,
+        vegetables.PROCNT,
+        vegetables.TOCPHA,
+        vegetables.CHOCDF,
+        vegetables.FIBTG,
+        vegetables.VITB6A,
+        vegetables.VITB12,
+        vegetables.K,
+        vegetables.NA,
+        vegetables.SUGAR,
+        vegetables.CA,
+    ]
+
     return (
         <>
-            <p>hello</p>
+            <h2>Nutrition Facts</h2>
+            <p>{params}</p>
+            <table>
+                <tbody>
+                    {arr.map((el, index) =>
+                        el !== undefined ? (
+                            <tr key={index}>
+                                <td>
+                                    <Nutrition nutrition={el} />
+                                </td>
+                            </tr>
+                        ) : (
+                            ''
+                        )
+                    )}
+                </tbody>
+            </table>
         </>
     )
 }
